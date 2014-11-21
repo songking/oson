976317e2cf3799cb4b619b81df5098223ff12a34
Yujun Cho <yujuncho7@gmail.com>
2014-11-21 21:10:55 +0800

    added base file for models

---
+++ Oson/Models/ItemEntry.h
@@ -0,0 +1,13 @@
+//
+//  ItemEntry.h
+//  Oson
+//
+//  Created by Yujun Cho on 11/21/14.
+//  Copyright (c) 2014 nick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemEntry : NSObject
+
+@end
---
+++ Oson/Models/ItemEntry.m
@@ -0,0 +1,13 @@
+//
+//  ItemEntry.m
+//  Oson
+//
+//  Created by Yujun Cho on 11/21/14.
+//  Copyright (c) 2014 nick. All rights reserved.
+//
+
+#import "ItemEntry.h"
+
+@implementation ItemEntry
+
+@end
